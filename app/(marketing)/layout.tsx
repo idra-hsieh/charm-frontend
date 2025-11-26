9cc01967aa5416ee0cc0ab7257b0338d5454e8b9
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-26 10:01:00 +0800

    chore: organize components

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "../globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/layout/Header";
 
 const inter = Inter({
   variable: "--font-inter",
