b09ade3c05c7b54c81122e2100a5be6eb2d21242
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-26 11:04:55 +0800

    feat(layout): structure header/footer files

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "../globals.css";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/header/Header";
 
 const inter = Inter({
   variable: "--font-inter",
