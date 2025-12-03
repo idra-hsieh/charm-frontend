f1ec96511f9d3950785718aaa238a79511c554ce
Idra Hsieh <idra.hsieh@gmail.com>
2025-12-03 22:08:25 +0800

    chore(i18n): optimize Chinese footer translation

--- components/features/cmi/result/CMIResultHeader.tsx
+++ components/features/cmi/result/CMIResultHeader.tsx
@@ -1,27 +1,8 @@
-import { useTranslations } from "next-intl";
+import React from 'react'
 
-type Props = {
-    current: number;
-    total: number;
-    onPrevious: () => void;
-}
-
-function CMIResultHeader({ current, total, onPrevious }: Props) {
-  const t = useTranslations("cmi.ui");
-    
+function CMIResultHeader() {
   return (
-      <div className="w-full h-[200px] bg-marble rounded-b-3xl text-foreground">
-          <div className='flex flex-col items-center justify-center text-center h-full space-y-2 mt-1'>
-              <h1 className='font-bold text-foreground/90 text-xl'>{t('header_title')}</h1>
-              <p className='text-xs text-foreground/65 max-w-[320px]'>
-                  <span className='font-semibold'>
-                      {t('header_desc_1')}
-                  </span>
-                  {" "}
-                  {t('header_desc_2')}
-              </p>
-          </div>
-     </div>
+    <div>CMIResultHeader</div>
   )
 }
 
